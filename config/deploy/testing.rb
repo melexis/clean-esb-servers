1e7fa87387614771b1b6b0307d7114099075686e
Lien Deboosere <leb@melexis.com>
2013-12-02 17:22:06 +0100

    add esb-servers in kuching and colo

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -7,4 +7,7 @@ set :ssh_options, {
 
 role :esb, %w{esb-a-test.sensors.elex.be esb-b-test.sensors.elex.be 
               esb-b-test.erfurt.elex.be
-              esb-a-test.sofia.elex.be esb-b-test.sofia.elex.be}
+              esb-a-test.sofia.elex.be esb-b-test.sofia.elex.be
+              esb-a-test.kuching.elex.be esb-b-test.kuching.elex.be
+              esb-a-test.colo.elex.be esb-b-test.colo.elex.be
+}
