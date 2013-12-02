92507ea4a94d197aaf498031b918a459fff67409
Brecht Hoflack <brh@melexis.com>
2013-12-02 16:25:05 +0100

    Remove esb-a-test.erfurt as cfengine isn't running there

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -6,5 +6,5 @@ set :ssh_options, {
 }
 
 role :esb, %w{esb-a-test.sensors.elex.be esb-b-test.sensors.elex.be 
-              esb-a-test.erfurt.elex.be esb-b-test.erfurt.elex.be
+              esb-b-test.erfurt.elex.be
               esb-a-test.sofia.elex.be esb-b-test.sofia.elex.be}
