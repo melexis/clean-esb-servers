b39e183741e76b8129067513eee511d01ff562b5
Lien Deboosere <leb@melexis.com>
2014-01-29 09:17:52 +0100

    forgot to add ssh options for role karaf

--- config/deploy/testing_kuching.rb
+++ config/deploy/testing_kuching.rb
@@ -8,4 +8,25 @@ set :ssh_options, {
 role :esb, %w{esb-a-test.kuching.elex.be esb-b-test.kuching.elex.be
 }
 
-role :karaf, %w{10.60.112.35 10.60.112.34}
+
+server '10.60.112.35',
+    user: 'smx',
+    roles: %{karaf},
+    ssh_options:
+        { auth_methods: ["password"],
+          password: 'smx',
+          port: 8101,
+          paranoid: false
+        }
+
+
+server '10.60.112.34',
+    user: 'smx',
+    roles: %{karaf},
+    ssh_options:
+        { auth_methods: ["password"],
+          password: 'smx',
+          port: 8101,
+          paranoid: false
+        }
+
